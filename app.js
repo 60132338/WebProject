d900c9bab8c469fcc46c33186be2c84afe128fcf
60132338 <tmdqja7931@naver.com>
2016-11-25 14:49:16 +0900

    16.11.25 commit

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use(function(req, res, next) {
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
